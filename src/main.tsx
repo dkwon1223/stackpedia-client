b3b4a14d55493671584b0a1c4d4cad8806b0b76c
David Kwon <112133897+dkwon1223@users.noreply.github.com>
2025-09-22 20:21:04 -0600

    Setup root layout

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router'
-import RootLayout from '@pages/RootLayout/RootLayout'
+import { createBrowserRouter, RouterProvider } from 'react-router';
+import RootLayout from '@pages/RootLayout/RootLayout';
 import './index.css';
 
 const router = createBrowserRouter([
